4342549666f6526e11d688434682242a190e617c
raouf <cheribetraouf607@gmail.com>
2023-12-23 22:19:53 +0100

    Auth0 implementation

--- front/App.js
+++ front/App.js
@@ -1,20 +1,29 @@
-
+// App.js
+import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { Auth0Provider } from '@auth0/auth0-react';
 import Login from './components/Login/Login';
 import HomePage from './components/HomePage/HomePage';
+
 const Stack = createStackNavigator();
+
 export default function App() {
   return (
-
-    <NavigationContainer>
+    <Auth0Provider
+      domain="dev-xhimeco72yx4icoh.us.auth0.com"
+      clientId="jwZzDuh0huFmRRf2e3s1069dXZENI7cv"
+      redirectUri="http://localhost:19006"
+    >
+      <NavigationContainer>
         <Stack.Navigator initialRouteName="Login">
-     
-       <Stack.Screen name="Login" component={Login} options={{ headerShown: false }} />
-       <Stack.Screen name="Home" component={HomePage}  />
-       </Stack.Navigator>
-    </NavigationContainer>
+          <Stack.Screen name="Login" component={Login} options={{ headerShown: false }} />
+          <Stack.Screen name="Home" component={HomePage} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Auth0Provider>
   );
 }
+
 
 
--- front/components/Login/Login.jsx
+++ front/components/Login/Login.jsx
@@ -3,6 +3,7 @@ import { View, Text, TouchableOpacity } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import { StyleSheet } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import { useAuth0 } from '@auth0/auth0-react';
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -26,35 +27,39 @@ const styles = StyleSheet.create({
     },
     
   });
-const Login = () => {
-
-  const navigation = useNavigation();
-
-  const handleIconPress = () => {
-    // Navigate to the HomePage component
-    navigation.navigate('Home'); // Replace 'HomePage' with the actual name of your HomePage component in the navigation stack
-  };
+  const Login = () => {
+    const { loginWithRedirect } = useAuth0();
+  
+    const handleLogin = async (provider) => {
+      try {
+        await loginWithRedirect({
+          screen_hint: provider,
+        });
+      } catch (error) {
+        console.error('Authentication failed: ', error);
+      }
+    };
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Welcome to Your Todolist</Text>
       {/* Icons of the famous authentication methods */}
       <View style={styles.logoContainer}>
-        <TouchableOpacity style={[styles.logoIcon]} onPress={handleIconPress} >
+        <TouchableOpacity style={[styles.logoIcon]} onPress={() => handleLogin('google-oauth2')} >
           <Text>
             <Icon name="google" size={30} color="#4285F4" /> {/* Gmail */}
           </Text>
         </TouchableOpacity>
-        <TouchableOpacity style={[styles.logoIcon]} onPress={handleIconPress}>
+        <TouchableOpacity style={[styles.logoIcon]} >
           <Text>
             <Icon name="apple" size={30} color="#4285F4" /> {/* Apple */}
           </Text>
         </TouchableOpacity>
-        <TouchableOpacity style={[styles.logoIcon]} onPress={handleIconPress}>
+        <TouchableOpacity style={[styles.logoIcon]} >
           <Text>
             <Icon name="facebook" size={30} color="#4285F4" /> {/* Facebook */}
           </Text>
         </TouchableOpacity>
-        <TouchableOpacity style={[styles.logoIcon]} onPress={handleIconPress}>
+        <TouchableOpacity style={[styles.logoIcon]} >
           <Text>
             <Icon name="twitter" size={30} color="#4285F4" /> {/* Twitter */}
           </Text>
