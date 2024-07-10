0becc04fdfd34381d8705f0cfb1725adb23fe8c5
Raouf878 <cheribetraouf607@gmail.com>
2024-07-10 14:33:30 +0200

    logout/persistence fixed

--- TodoList/src/screens/HomeScreen.tsx
+++ TodoList/src/screens/HomeScreen.tsx
@@ -1,17 +1,39 @@
 import React from "react";
 import { View, Text, Button, StyleSheet } from "react-native";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import auth from '@react-native-firebase/auth';
+import { GoogleSignin } from '@react-native-google-signin/google-signin';
+import { useDispatch } from 'react-redux';
+import { setUserid } from '../redux/slices/Credentials';
 
 const HomeScreen = () => {
-  const handleLogout = () => {
-    // Add your logout logic here
-    console.log("Logout button pressed");
+  const dispatch=useDispatch()
+  const logout = async () => {
+    try {
+      // Sign out from Firebase
+      await auth().signOut();
+  
+      // Sign out from Google
+      await GoogleSignin.signOut();
+  
+      // Remove user data from AsyncStorage
+      await AsyncStorage.removeItem('user');
+  
+      // Clear user ID in Redux store
+      dispatch(setUserid(null));
+  
+      // Navigate to Login screen
+      
+    } catch (error) {
+      console.error('Logout error:', error);
+    }
   };
 
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Welcome to HomeScreen</Text>
       <View style={styles.buttonContainer}>
-        <Button title="Logout" onPress={handleLogout} />
+        <Button title="Logout" onPress={logout} />
       </View>
     </View>
   );
